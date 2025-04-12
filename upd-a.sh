0d86b9dd5b7bf80325d6d279a1d34ab9fd8fa840
Haley Euphemia Sands <haleyashley@live.com.au>
2025-04-12 19:16:53 +1000

    added python-mpv to the pip updates

--- upd-a.sh
+++ upd-a.sh
@@ -3,6 +3,7 @@ mangadex-dl --update;
 pip install -U requests; # A dependency of mangadex-dl.
 pip install -U yt-dlp;
 pip install -U himawari-api;
+pip install -U  python-mpv;
 rustup update;
 flatpak update;
 sudo apt update;
