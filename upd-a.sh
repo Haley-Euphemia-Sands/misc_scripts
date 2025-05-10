398cf3b60346832dfa19aa3ad684c5d8bf3e79ba
Haley Euphemia Sands <haleyashley@live.com.au>
2025-05-10 21:29:04 +1000

    Added homebrew package updating to the script for upgrading packages

--- upd-a.sh
+++ upd-a.sh
@@ -4,8 +4,11 @@ pip install -U requests; # A dependency of mangadex-dl.
 pip install -U yt-dlp;
 pip install -U himawari-api;
 pip install -U  python-mpv;
+pip install -U sherlock-project;
 rustup update;
 flatpak update;
+brew update;
+brew upgrade;
 sudo apt update;
 sudo apt upgrade;
 sudo update-grub;
